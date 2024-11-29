654169c6b5a86ccf3122cc30b509a6d02efb019e
heawer <accounts+ghtest@sapphic.moe>
2024-11-29 19:02:55 +0500

    fix faker

--- store/apps.py
+++ store/apps.py
@@ -1,22 +1,24 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 from faker import Faker
 
 fake = Faker()
 
-def populate(n):
+def populate(sender, **kwargs):
     from store.models import Dish
     
-    for _ in range(n):
-        Dish.objects.create(
-            name=fake.name(),
-            description=fake.text(),
-            price=fake.pyint(100, 6000),
-            is_available=fake.boolean()
-        )
+    if not Dish.objects.exists():
+        for _ in range(10):
+            Dish.objects.create(
+                name=fake.name(),
+                description=fake.text(),
+                price=fake.pyint(100, 6000),
+                is_available=fake.boolean()
+            )
 
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
     
     def ready(self):
-        populate(1)
+        post_migrate.connect(populate, sender=self)
